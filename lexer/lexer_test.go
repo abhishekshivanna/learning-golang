5697d14e21e5fed2005b419e35ac2c241539f73f
Abhishek Shivanna <abhisheks91@gmail.com>
2018-10-22 10:16:18 -0700

    Added peek to lexer and new keywords

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -45,7 +45,8 @@ func TestNextTokent(t *testing.T) {
 		x + y;
 	};
 	
-	let result = add(five, ten);`
+	let result = add(five, ten);
+	`
 
 	tests := []struct {
 		expectedType    token.TokenType
@@ -102,4 +103,65 @@ func TestNextTokent(t *testing.T) {
 		}
 	}
 
+}
+
+func TestNewMathTokens(t *testing.T) {
+
+	input := ` 
+	!-/*5;
+	s < 10 > 5;
+
+	if (5 < 10) {
+		return true;
+	} else {
+		return false;
+	}
+	`
+
+	tests := []struct {
+		expectedType    token.TokenType
+		expectedLiteral string
+	}{
+		{token.BANG, "!"},
+		{token.MINUS, "-"},
+		{token.SLASH, "/"},
+		{token.ASTERISK, "*"},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+		{token.IDENT, "s"},
+		{token.LT, "<"},
+		{token.INT, "10"},
+		{token.GT, ">"},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+		{token.IF, "if"},
+		{token.LPAREN, "("},
+		{token.INT, "5"},
+		{token.LT, "<"},
+		{token.INT, "10"},
+		{token.RPAREN, ")"},
+		{token.LBRACE, "{"},
+		{token.RETURN, "return"},
+		{token.TRUE, "true"},
+		{token.SEMICOLON, ";"},
+		{token.RBRACE, "}"},
+		{token.ELSE, "else"},
+		{token.LBRACE, "{"},
+		{token.RETURN, "return"},
+		{token.FALSE, "false"},
+		{token.SEMICOLON, ";"},
+		{token.RBRACE, "}"},
+	}
+
+	l := New(input)
+
+	for i, tt := range tests {
+		tok := l.NextToken()
+		if tok.Type != tt.expectedType {
+			t.Fatalf("tests[%d] - tokentype worng. expected=%q, got=%q", i, tt.expectedType, tok.Type)
+		}
+		if tok.Literal != tt.expectedLiteral {
+			t.Fatalf("tests[%d] - literal worng. expected=%q, got=%q", i, tt.expectedLiteral, tok.Literal)
+		}
+	}
 }
